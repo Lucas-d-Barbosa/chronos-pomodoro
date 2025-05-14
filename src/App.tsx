a3f9510aa70a97daedeb5c15825b43bb8730a994
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-05-14 14:52:08 -0300

    feat: Compontes de ciclos adicionados (Cycles)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { CountDown } from "./components/CountDown";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { DefaultInput } from "./components/DefaultInput";
-
+import { Cycles } from "./components/Cycles";
 import "./styles/global.css";
 import "./styles/theme.css";
 
@@ -30,8 +30,7 @@ export function App() {
           </div>
 
           <div className="formRow">
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0</p>
+            <Cycles />
           </div>
 
           <div className="formRow">
