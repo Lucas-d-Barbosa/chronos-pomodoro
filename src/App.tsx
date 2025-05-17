eb53b0c47a67c57f622b53145c9f77d64bcd0417
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-05-16 21:53:53 -0300

    feat: Iniciando a criação dos temas da aplicação (dark e light)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Container } from "./components/Container";
-import { Heading } from "./components/Heading";
+// import { Heading } from "./components/Heading";
 import { CountDown } from "./components/CountDown";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
@@ -10,23 +10,10 @@ import { DefaulButton } from "./components/DefaultButton";
 import { PlayCircleIcon } from "lucide-react";
 import "./styles/global.css";
 import "./styles/theme.css";
-import { useState } from "react";
 
 export function App() {
-  // const [numero, setNumero] = useState(() => {
-  //   console.log("Lazy initialization");
-  //   return 0;
-  // });
-  const [numero, setNumero] = useState(0);
-
-  function handleClick(): void {
-    setNumero((prevState) => prevState + 1);
-  }
-
   return (
     <>
-      <Heading>Número: {numero}</Heading>
-      <button onClick={handleClick}>Aumenta Número</button>
       <Container>
         <Logo />
       </Container>
@@ -42,11 +29,7 @@ export function App() {
       <Container>
         <form action="" className="form">
           <div className="formRow">
-            <DefaultInput
-              id="meuInput"
-              labeltext={numero.toString()}
-              type="text"
-            />
+            <DefaultInput id="meuInput" labeltext="Task" type="text" />
           </div>
 
           <div className="formRow">
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,47 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./style.module.css";
+import { useState } from "react";
+type AvailableThemes = "dark" | "light";
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+  function handleThemeChange(e: React.MouseEvent): void {
+    e.preventDefault();
+    if (theme === "dark") {
+      setTheme("light");
+      return;
+    }
+    setTheme("dark");
+  }
   return (
     <>
+      <h1>{theme}</h1>
       <nav className={styles.menu}>
-        <a href="#" className={styles.menuLink}>
+        <a href="#" className={styles.menuLink} aria-label="Home" title="Home">
           <HouseIcon />
         </a>
-        <a href="#" className={styles.menuLink}>
+        <a
+          href="#"
+          className={styles.menuLink}
+          aria-label="Histórico"
+          title="Histórico"
+        >
           <HistoryIcon />
         </a>
-        <a href="#" className={styles.menuLink}>
+        <a
+          href="#"
+          className={styles.menuLink}
+          aria-label="Configurações"
+          title="Configurações"
+        >
           <SettingsIcon />
         </a>
-        <a href="#" className={styles.menuLink}>
+        <a
+          href="#"
+          className={styles.menuLink}
+          aria-label="Alterar Tema"
+          title="Alterar Tema"
+          onClick={handleThemeChange}
+        >
           <SunIcon />
         </a>
       </nav>
