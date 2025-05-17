1c2e28e2fc3b4c864bec8aed33ed021aa56f35f8
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-05-17 17:01:17 -0300

    feat: Adicionada página básica de NotFound

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./styles/global.css";
 import "./styles/theme.css";
 import { Home } from "./pages/home";
+// import { NotFound } from "./pages/notFound";
 
 export function App() {
   return (
