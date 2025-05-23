bcc9dc2743e9cfea9d0569a7e3ab3aa246b970f9
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-05-23 18:34:57 -0300

    feat: Melhoria da organização dos arquivos do TaskContext

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,20 +2,10 @@ import { DefaultInput } from "../DefaultInput";
 import { Cycles } from "../Cycles";
 import { DefaulButton } from "../DefaultButton";
 import { PlayCircleIcon } from "lucide-react";
-import { useTaskContext } from "../../contexts/TaskContext";
 
 export function MainForm() {
-  const { state, setState } = useTaskContext();
-  function handleClick() {
-    setState((prevState) => {
-      return { ...prevState, formattedSecondsRemainig: "02:20" };
-    });
-  }
   return (
     <form action="" className="form">
-      <button type="button" onClick={handleClick}>
-        Clicar
-      </button>
       <div className="formRow">
         <DefaultInput id="meuInput" labeltext="Task" type="text" />
       </div>
