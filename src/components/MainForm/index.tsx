c68ecb2a1acb31c5f703b2cbbfbf63e53a494bda
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-05-23 22:19:25 -0300

    feat: Camputa do valor do input de maneira controlada com onChange e useState

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,26 @@ import { DefaultInput } from "../DefaultInput";
 import { Cycles } from "../Cycles";
 import { DefaulButton } from "../DefaultButton";
 import { PlayCircleIcon } from "lucide-react";
+import { useState } from "react";
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState("");
   function handleCreateNewTask(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log("Deu certo.");
+    console.log("Deu certo.", taskName);
   }
   return (
     <form action="" className="form" onSubmit={handleCreateNewTask}>
       <div className="formRow">
-        <DefaultInput id="meuInput" labeltext="Task" type="text" />
+        <DefaultInput
+          id="meuInput"
+          labeltext="Task"
+          type="text"
+          value={taskName}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTaskName(e.target.value)
+          }
+        />
       </div>
 
       <div className="formRow">
