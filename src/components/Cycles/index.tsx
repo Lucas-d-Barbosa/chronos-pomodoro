51db6adbf058f816b7c05cf51f206e99ed405a7e
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-05-24 15:57:03 -0300

    feat: Criação da renderização da lista de ciclos.

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,19 +1,35 @@
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
 import styles from "./style.module.css";
 
 export function Cycles() {
+  const { state } = useTaskContext();
+  const cycleStap = Array.from({ length: state.currentCycle });
+  const cycleDescriptionMap = {
+    workTime: "tempo de trabalho.",
+    shortBreakTime: "tempo de descanso curto.",
+    longBreakTime: "tempo de descanso longo.",
+  };
   return (
     <div className={styles.cycles}>
       <span>Ciclos:</span>
 
       <div className={styles.cyclesDots}>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.longBreakTime}`}></span>
+        {cycleStap.map((_, index) => {
+          const nextCycle = getNextCycle(index);
+          const nextCycleType = getNextCycleType(nextCycle);
+          return (
+            <span
+              aria-label={`Indicador de ${cycleDescriptionMap[nextCycleType]}`}
+              title={`Indicador de ${cycleDescriptionMap[nextCycleType]}`}
+              key={`${nextCycleType}_${nextCycle}`}
+              className={`${styles.cycleDot} ${styles[nextCycleType]}`}
+            ></span>
+          );
+        })}
+
+        {/* <span className={`${styles.cycleDot} ${styles.workTime}`}></span> */}
       </div>
     </div>
   );
--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -67,10 +67,11 @@ export function MainForm() {
         <p>Próximo intervalo é de 25 min</p>
       </div>
 
-      <div className="formRow">
-        <Cycles />
-      </div>
-
+      {state.currentCycle > 0 && (
+        <div className="formRow">
+          <Cycles />
+        </div>
+      )}
       <div className="formRow">
         <DefaulButton type="submit" icon={<PlayCircleIcon />} />
       </div>
