c0d8560ee444c7036e76ebac4f6e8948c8a88514
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-05-29 19:01:01 -0300

    feat: Adicionando o salvamento das tasks no localStorage e suas recuperações.

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -5,13 +5,25 @@ import { taskReduce } from "./taskReduce";
 import { TimerWorkerMenager } from "../../workes/TimerWorkerManager";
 import { TaskActionTypes } from "./TaskActions";
 import { loadBeep } from "../../utils/loadBeep";
+import type { TaskStateModel } from "../../models/TaskStateModel";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
 };
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
-  const [state, dispatch] = useReducer(taskReduce, initialTaskState);
+  const [state, dispatch] = useReducer(taskReduce, initialTaskState, () => {
+    const storageState = localStorage.getItem("state");
+
+    if (!storageState) return initialTaskState;
+    const parsedStorageState = JSON.parse(storageState) as TaskStateModel;
+    return {
+      ...parsedStorageState,
+      activeTask: null,
+      secondsRemainig: 0,
+      formattedSecondsRemainig: "00:00",
+    };
+  });
   const worker = TimerWorkerMenager.getInstance();
   const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
@@ -34,6 +46,8 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
     }
   });
   useEffect(() => {
+    // Estado mudou?
+    localStorage.setItem("state", JSON.stringify(state));
     if (!state.activeTask) {
       worker.terminate();
     }
