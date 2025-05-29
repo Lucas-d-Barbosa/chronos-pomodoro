cec3307c0e19f8bd8398b9f34005f32848500b31
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-05-29 19:52:35 -0300

    feat: Formatando as datas com a lib date-fns

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -6,6 +6,7 @@ import { DefaulButton } from "../../components/DefaultButton";
 
 import styles from "./Styles.module.css";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { formatDate } from "../../utils/formatDate";
 export function History() {
   const { state } = useTaskContext();
   return (
@@ -42,8 +43,10 @@ export function History() {
                     <tr key={task.id}>
                       <td>{task.name}</td>
                       <td>{task.duration} min</td>
-                      <td>{new Date(task.startDate).toISOString()}</td>
-                      <td>{task.interruptDate}</td>
+                      <td>{formatDate(task.startDate)}</td>
+                      <td>
+                        {formatDate(task.interruptDate ?? task.startDate)}
+                      </td>
                       <td>{task.type}</td>
                     </tr>
                   );
