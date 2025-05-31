bae458d47257b852c9943576ad79f4682ba2c0b0
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-05-30 21:27:58 -0300

    fix Correção TaskActions

--- src/contexts/TaskContext/TaskActions.ts
+++ src/contexts/TaskContext/TaskActions.ts
@@ -1,51 +1,47 @@
-// useReduce <- hook do React que recebe um reduce e um estado inicial
-// reducer <- função que recebe o estado atual e uma ação e retorna o estado
-// state <- o estado atual
-// action <- a ação disparada, geralmente é um objeto com type e (opcionalmetnte) payload
-// type <- o tipo da ação, geralmente uma string (pode ser um enum, constrante, etc)
-// payload <- os dados extras enviados junto com a action, se necessário para atualziar o estado
-
 import type { TaskModel } from "../../models/TaskModel";
 import type { TaskStateModel } from "../../models/TaskStateModel";
 
-export enum TaskActionTypes {
-  START_TASK = "START_TASK",
-  INTERRUPT_TASK = "INTERRUPT_TASK",
-  PAUSE_TASK = "PAUSE_TASK",
-  RESET_STATE = "RESET_STATE",
-  COUNT_DOWN = "COUNT_DOWN",
-  COMPLETE_TASK = "COMPLETE_TASK",
-  RESUME_TASK = "RESUME_TASK",
-  CHANGE_SETTINGS = "CHANGE_SETTINGS",
-}
+export const TaskActionTypes = {
+  START_TASK: "START_TASK",
+  INTERRUPT_TASK: "INTERRUPT_TASK",
+  PAUSE_TASK: "PAUSE_TASK",
+  RESET_STATE: "RESET_STATE",
+  COUNT_DOWN: "COUNT_DOWN",
+  COMPLETE_TASK: "COMPLETE_TASK",
+  RESUME_TASK: "RESUME_TASK",
+  CHANGE_SETTINGS: "CHANGE_SETTINGS",
+} as const;
+
+export type TaskActionTypeKeys =
+  (typeof TaskActionTypes)[keyof typeof TaskActionTypes];
 
 export type TaskActionModel =
   | {
-      type: TaskActionTypes.START_TASK;
+      type: typeof TaskActionTypes.START_TASK;
       payload: TaskModel;
     }
   | {
-      type: TaskActionTypes.COUNT_DOWN;
+      type: typeof TaskActionTypes.COUNT_DOWN;
       payload: { secondsRemaining: number };
     }
   | {
-      type: TaskActionTypes.INTERRUPT_TASK;
+      type: typeof TaskActionTypes.INTERRUPT_TASK;
     }
   | {
-      type: TaskActionTypes.PAUSE_TASK;
+      type: typeof TaskActionTypes.PAUSE_TASK;
       payload: TaskModel | null;
     }
   | {
-      type: TaskActionTypes.RESUME_TASK; // ← adiciona este caso
+      type: typeof TaskActionTypes.RESUME_TASK;
       payload: TaskModel;
     }
   | {
-      type: TaskActionTypes.CHANGE_SETTINGS; // ← adiciona este caso
+      type: typeof TaskActionTypes.CHANGE_SETTINGS;
       payload: TaskStateModel["config"];
     }
   | {
-      type: TaskActionTypes.RESET_STATE;
+      type: typeof TaskActionTypes.RESET_STATE;
     }
   | {
-      type: TaskActionTypes.COMPLETE_TASK;
+      type: typeof TaskActionTypes.COMPLETE_TASK;
     };
